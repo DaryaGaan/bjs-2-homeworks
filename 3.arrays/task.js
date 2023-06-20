1c06b7fd50ff79b40cf1b32cd359e64b5921ac8b
Дарья Приходько <dev-0@mail.ru>
2023-06-20 13:02:52 +0600

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])) {
+    return true;
+  } else {
+return false;
+}
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  if ((users.length == 0) || !((gender === "мужской") || (gender === "женский"))) {
+    return 0;
+  }
+
+  if (gender === "мужской") {
+    let result = users.filter(user => user.gender === "мужской").map(user => user.age)
+    return result.reduce((acc, item) => acc + item) / result.length;
+  }
+    else if (gender === "женский") {
+      let result = users.filter(user => user.gender === "женский").map(user => user.age)
+    return result.reduce((acc, item) => acc + item) / result.length;
+  } 
 }
